34e949805a482bf504b27d4a0a8809de7cb5e740
olektrolek <gosiawiktor2410@gmail.com>
2026-04-23 22:31:21 +0200

    distutils fix

--- main.py
+++ main.py
@@ -4,6 +4,22 @@ import os
 import re
 from dotenv import load_dotenv
 
+# patch lib for distutils
+import site
+from pathlib import Path
+def patch_distutils():
+    for sp in site.getsitepackages():
+        f = Path(sp) / "undetected_chromedriver" / "patcher.py"
+        if f.exists():
+            text = f.read_text(encoding="utf-8")
+            fixed = text.replace(
+                "from distutils.version import LooseVersion",
+                "from setuptools._distutils.version import LooseVersion"
+            )
+            if text != fixed:
+                f.write_text(fixed, encoding="utf-8")
+patch_distutils()
+
 # global functions
 def is_valid_email(email: str) -> bool:
     pattern = r"^[\w\.-]+@[\w\.-]+\.\w{2,}$"
@@ -29,16 +45,16 @@ try:
             load_dotenv()
 
             FIELDS = {
-                0: ("- Login do Infocar: ", "Podaj nowy login:", "LOGIN", "Brak loginu do infocar"),
-                1: ("- Hasło do Infocar: ", "Podaj nowe hasło:", "LOGIN_PASSWORD", "Brak hasła do infocar"),
-                2: ("- Województwo: ", "Podaj nowe województwo:", "PROVINCE", "Brak województwa"),
-                3: ("- Ośrodek WORD: ", "Podaj nowy ośrodek WORD:", "CENTER", "Brak ośroda WORD"),
-                4: ("- Kategoria: ", "Podaj nową kategorię:", "CATEGORY", "Brak kategorii"),
+                0: ("- Login do Infocar: ", "Podaj login:", "LOGIN", "Brak loginu do infocar"),
+                1: ("- Hasło do Infocar: ", "Podaj hasło:", "LOGIN_PASSWORD", "Brak hasła do infocar"),
+                2: ("- Województwo: ", "Podaj województwo:", "PROVINCE", "Brak województwa"),
+                3: ("- Ośrodek WORD: ", "Podaj ośrodek WORD:", "CENTER", "Brak ośroda WORD"),
+                4: ("- Kategoria: ", "Podaj kategorię:", "CATEGORY", "Brak kategorii"),
                 5: ("- Typ egzaminu: ", "Wybierz typ egzaminu (praktyka / teoria):", "EXAM_TYPE", "Brak typu egzaminu"),
                 6: ("- Częstotliwość pobierania terminów: ", "Podaj częstotliwość w minutach:", "MIN_FREQUENCY", "Brak częstotliwości pobierania terminów"),
                 7: ("- Ilość dni wyprzedzenia terminów: ", "Podaj liczbę dni wyprzedzenia terminów:", "DAYS_AHEAD", "Brak ilości dni wyprzedzania terminów"),
-                8: ("- Gmail (SMTP): ", "Podaj adres Gmail używany do SMTP:", "EMAIL", "Brak gmaila dla SMTP"),
-                9: ("- Hasło SMTP: ", "Podaj hasło SMTP:", "EMAIL_PASSWORD", "Brak hasła dla SMTP"),
+                8: ("- Gmail (SMTP): ", "Podaj adres Gmail do SMTP:", "EMAIL", "Brak gmaila dla SMTP"),
+                9: ("- Hasło SMTP: ", "Podaj hasło do SMTP:", "EMAIL_PASSWORD", "Brak hasła dla SMTP"),
             }
             def envPrint(index):
                 return FIELDS.get(index, (None, None, None))
